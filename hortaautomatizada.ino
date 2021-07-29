a6b944287d4e181f1790e08f97c68a14ef614364
TTT-zano <thomtrize@gmail.com>
2021-07-28 21:39:42 -0300

    Hortacode

--- hortaautomatizada.ino
+++ hortaautomatizada.ino
@@ -4,15 +4,16 @@
 //definição dos pinos digitais e analógicos
 #define pin_analog A0
 #define pin2_analog A1
-#define relay 8
 #define p5va0 7
 #define p5va1 6
+#define relay 8
 #define ledred 10
 #define ledgreen 9
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
+int contador_i, contador_e, soma;
 //variavel tipo int para conversão dos valores dos sensores
-int ValAnalogIn;
+int ValAnalogIn, ValAnalogIn2;
 void setup(){
 //configuração do lcd com as definições de 16 caracteres por 2 linhas
   lcd.begin(16, 2);
@@ -30,7 +31,11 @@ void setup(){
 void loop(){
   //define pino 5V A0 e A1 em modo HIGH(ligado) fornecendo alimentação 5V pela porta digital
   digitalWrite(p5va0, HIGH);
-  digitalWrite(p5va1, HIGH);
+  digitalWrite(p5va0, HIGH);
+
+  contador_i = 0;
+  contador_e = 0;
+  soma = 0;
 //limpa o lcd para receber novos dados
   lcd.clear();
   
@@ -39,8 +44,8 @@ void loop(){
 //converção dos valores dos sensores para porcentagem
   int Porcento = map(ValAnalogIn, 1023, 0, 0, 100);
 // configuração do segundo sensor
-  ValAnalogIn = analogRead(pin2_analog);
-  int Porcento2 = map(ValAnalogIn, 1023, 0, 0, 100);
+  ValAnalogIn2 = analogRead(pin2_analog);
+  int Porcento2 = map(ValAnalogIn2, 1023, 0, 0, 100);
   
 // configuração do display lcd
 /* modelo de print dos sensores com a seguinte palavra "S1: XXX% S2: XXX%"
@@ -54,18 +59,18 @@ obs: XXX remetem aos valores dos sensores de 0% a 100%*/
   lcd.setCursor(8, 0);
   lcd.print("S2:");
   lcd.setCursor(12, 0);
-  lcd.print(Porcento);
+  lcd.print(Porcento2);
   lcd.setCursor(15, 0);
   lcd.print("%");
 
-/*Caso os sensores forem igualmente iguais ou menores de 45% 
-o módulo relê aciona a válvula solenóide*/
+/*Caso um sensor for igual ou menor de 45% 
+o módulo relê aciona a válvula solenoide*/
     if(Porcento <= 45 || Porcento2 <= 45){
           digitalWrite(ledred, HIGH);
           digitalWrite(relay, LOW);
           lcd.setCursor(3, 1);
           lcd.print("Irrigando...");
-          delay(300000);
+          ++contador_i;       
       }
 
 /*Caso os valores dos dois sensores ou somente um deles possuir 
@@ -75,12 +80,18 @@ o valor maior que 45% ele mantem o relê desligado*/
           digitalWrite(relay, HIGH);
           lcd.setCursor(3, 1);
           lcd.print("Irrigado...");
+          ++contador_e;
       }
 
-//desativa os sensores de umidade para evitar gasto do sistema
-  digitalWrite(p5va0, LOW);
-  digitalWrite(p5va1, LOW);
+    soma = contador_i + contador_e;
+    if(soma == 2){
+//desativa os sensores para evitar gasto do sistema
+        digitalWrite(p5va0, LOW);
+        digitalWrite(p5va1, LOW);
+        digitalWrite(relay, HIGH);
+        delay(300000);
+    }
 
-//espera 15 minutos para retomar o loop
-delay(900000);
+//espera 1 segundo para retomar o loop
+delay(1000);
 }
